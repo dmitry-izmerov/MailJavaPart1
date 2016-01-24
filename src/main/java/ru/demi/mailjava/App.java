a03d4bf3a1917513984aa0409828624e2c282e0c
demi <idd90i@gmail.com>
2016-01-24 17:24:23 +0300

    init

--- src/main/java/ru/demi/mailjava/App.java
+++ src/main/java/ru/demi/mailjava/App.java
@@ -1,22 +1,44 @@
 package ru.demi.mailjava;
 
+import ru.demi.mailjava.accounts.AccountService;
+import ru.demi.mailjava.accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import ru.demi.mailjava.servlets.AllRequestsServlet;
+import ru.demi.mailjava.servlets.SessionsServlet;
+import ru.demi.mailjava.servlets.UsersServlet;
 
 /**
- * Created by demi on 21.01.16.
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        AccountService accountService = new AccountService();
 
-        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        contextHandler.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        // Order handlers is important for Jetty.
+        handlers.setHandlers(new Handler[]{resourceHandler, context});
 
         Server server = new Server(8080);
-        server.setHandler(contextHandler);
+        server.setHandler(handlers);
+
         server.start();
         server.join();
     }
---
+++ src/main/java/ru/demi/mailjava/accounts/AccountService.java
@@ -0,0 +1,38 @@
+package ru.demi.mailjava.accounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author demi
+ * Created on 22.01.16
+ */
+public class AccountService {
+    private final Map<String, UserProfile> loginToProfile;
+    private final Map<String, UserProfile> sessionIdToProfile;
+
+    public AccountService() {
+        loginToProfile = new HashMap<>();
+        sessionIdToProfile = new HashMap<>();
+    }
+
+    public void addNewUser(UserProfile userProfile) {
+        loginToProfile.put(userProfile.getLogin(), userProfile);
+    }
+
+    public UserProfile getUserByLogin(String login) {
+        return loginToProfile.get(login);
+    }
+
+    public UserProfile getUserBySessionId(String sessionId) {
+        return sessionIdToProfile.get(sessionId);
+    }
+
+    public void addSession(String sessionId, UserProfile userProfile) {
+        sessionIdToProfile.put(sessionId, userProfile);
+    }
+
+    public void deleteSession(String sessionId) {
+        sessionIdToProfile.remove(sessionId);
+    }
+}
